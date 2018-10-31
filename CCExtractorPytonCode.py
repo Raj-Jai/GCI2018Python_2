06d36f98758bf6c826ab253938b7718317f9c1e9
Raj-Jai <44637563+Raj-Jai@users.noreply.github.com>
2018-10-31 17:21:51 +0530

    Add files via upload

---
+++ CCExtractorPytonCode.py
@@ -0,0 +1,15 @@
+Google = "GCI is great."
+x = 0
+for loop in Google:
+    print(Google)
+    x = x + 1
+    if x == 10:
+      break
+
+    
+print("Please Enter Your Name: ")
+name = input()
+
+reverse = name[::-1]
+
+print("Hello {},please to meet you! Did you know that. your name backwards is" .format (name),(reverse))
